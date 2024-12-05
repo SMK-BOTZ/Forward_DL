8edb4d242756c833b651fef37f4034d95e412775
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-12-06 02:35:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
     
     API_ID = environ.get("API_ID", "28243586")
     API_HASH = environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7064899225:AAF80y8D2VbmKb8S_k5PgKBq5hjRV5L3Rcs") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7242493073:AAFZ2FOAshfsAHiP-dzChR6FqmpZZN7_dWQ") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6551906246').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://sahilkaleech:ffmFFKUQXBFDgtxr@cluster0.fn00j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://smkbotz:Ur3UZGbmzAvq9ig0@cluster0.s2vqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002056677294'))
